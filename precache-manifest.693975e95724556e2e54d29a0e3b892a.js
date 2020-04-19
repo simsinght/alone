d868a436b1462704fb663d6ce77b4fe5d83501c7
Sim Singh <simsinght@gmail.com>
2020-04-19 00:53:51 -0700

    Updates

---
+++ precache-manifest.693975e95724556e2e54d29a0e3b892a.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c97f035afe302d6f8851ad680fe866d4",
+    "url": "/alone/index.html"
+  },
+  {
+    "revision": "75227a0acc4cd710e520",
+    "url": "/alone/static/css/2.771aec41.chunk.css"
+  },
+  {
+    "revision": "d6bfa05ad5dad49b7b0d",
+    "url": "/alone/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "75227a0acc4cd710e520",
+    "url": "/alone/static/js/2.dc34d38b.chunk.js"
+  },
+  {
+    "revision": "63480c753a700745c58375d8a6d2c66e",
+    "url": "/alone/static/js/2.dc34d38b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6bfa05ad5dad49b7b0d",
+    "url": "/alone/static/js/main.5fdfd637.chunk.js"
+  },
+  {
+    "revision": "96de517df6b154b67527",
+    "url": "/alone/static/js/runtime-main.fafb65b5.js"
+  },
+  {
+    "revision": "d18ffbd15176281615ac82e29cf2c3f3",
+    "url": "/alone/static/media/border.d18ffbd1.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/alone/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/alone/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/alone/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/alone/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/alone/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/alone/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/alone/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/alone/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/alone/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/alone/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/alone/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/alone/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/alone/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/alone/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/alone/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/alone/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "af94bbad81712d1caeeaf293b21314a6",
+    "url": "/alone/static/media/red-dot-e.af94bbad.png"
+  },
+  {
+    "revision": "2d2501fcaa27c1080c7706fe4070f375",
+    "url": "/alone/static/media/red-dot-n.2d2501fc.png"
+  },
+  {
+    "revision": "e9ccb056d6f2a2257ca2273488003b55",
+    "url": "/alone/static/media/red-dot-ne.e9ccb056.png"
+  },
+  {
+    "revision": "3ad1de2f9d8e65ef47d4c997a32dec13",
+    "url": "/alone/static/media/red-dot-nw.3ad1de2f.png"
+  },
+  {
+    "revision": "9109295a3ac27bd1f096f978bdf19d3c",
+    "url": "/alone/static/media/red-dot-s.9109295a.png"
+  },
+  {
+    "revision": "64bb45ff7114ddfe76987a32e617a03a",
+    "url": "/alone/static/media/red-dot-se.64bb45ff.png"
+  },
+  {
+    "revision": "cb8a09d15676c8f62fdca06ca62cf948",
+    "url": "/alone/static/media/red-dot-sw.cb8a09d1.png"
+  },
+  {
+    "revision": "f623980dc102f96f784d8d90be4f012c",
+    "url": "/alone/static/media/red-dot-w.f623980d.png"
+  },
+  {
+    "revision": "3adb37ac1416dab565da69d3e08ae8d2",
+    "url": "/alone/static/media/title.3adb37ac.png"
+  }
+]);
